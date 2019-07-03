eb5cc2934bf26773d94ad32f81524f90f534605e
Sébastien Besnier <sebastien.fabrice.besnier@gmail.com>
2019-07-03 22:41:20 +0200

    Added questions about Elm

--- webapp/scripts/create_chapters.py
+++ webapp/scripts/create_chapters.py
@@ -21,25 +21,37 @@ CHAPTERS = [
                     " the attributes with a space; I'm not asking about"
                     " the values, give only the attributes!)? <br />"
                     " <code><pre>"
-                    "  &lt;p id='head' class='column-3' data-userid='42'>"
-                    "    Hello world!"
-                    "  &lt;/p>",
-                'grade_by_answer':{},
+                    "&lt;p id='hello' class='column-3' data-userid='42'>"
+                    "Hello world!"
+                    "&lt;/p>",
                 'coefficient': 0.5,
             },
             {
                 'title':
                     "Give three tags that belong to the head section"
                     " of an HTML page (i.e. between &lt;head> and &lt;/head>)."
-                    " Separate your answers by a space" ,
-                'grade_by_answer': {},
+                    " Separate your answers with a space." ,
+
+                'coefficient': 0.5,
+            },
+            {
+                'title':
+                    "Which one of the following is a <strong>valid</strong>"
+                    " piece of HTML (enter 1, 2, 3 or 4):<ol>"
+                    "<li><code>&lt;p>Lorem ipsum &lt;em>dolor &lt;strong>sit&lt;/strong>&lt;/em> amet&lt;/p></code></li>"
+                    "<li><code>&lt;p>Lorem ipsum &lt;html>dolor &lt;strong>sit&lt;/strong>&lt;/html> amet&lt;/p></code></li>"
+                    "<li><code>&lt;p>Lorem ipsum &lt;em>dolor &lt;strong>sit&lt;/em>&lt;/strong> amet&lt;/p></code></li>"
+                    "<li><code>&lt;p>Lorem ipsum &lt;em>dolor &lt;strong>sit&lt;/strong>&lt;em> amet&lt;/p></code></li>"
+                    "</ol>",
+
                 'coefficient': 0.5,
             },
             {
                 'title':
                     "Write a piece of HTML which displays \"Go to hell\""
-                    " and is a link to the website \"http://666.com\".",
-                'grade_by_answer': {},
+                    " such as the text is a link to the website"
+                    " \"http://666.com\".",
+
                 'coefficient': 0.5,
             },
         ],
@@ -48,13 +60,56 @@ CHAPTERS = [
         'id': '02-Elm',
         'name' : 'Elm',
         'end_date': date(2019, 9, 19),
-        'questions': [],
+        'questions': [
+            {
+                'title':
+                    "Let <code>f : String -> Int</code>"
+                    " and <code>g : List Float -> Int</code>"
+                    " be two functions. What is the type of"
+                    " <code>h</code> where:"
+                    " <pre>"
+                    "   <code>h a b = f a + g b</code>"
+                    " </pre>",
+                'coefficient': 1,
+            },
+            {
+                'title':
+                    "How to produce the following piece of HTML in Elm?"
+                    "<pre>"
+                    "  <code>"
+                    "&lt;ul>&lt;li>Hi Marvin!&lt;/li>&lt;/ul>"
+                    "  </code>"
+                    "</pre>",
+                'coefficient': 1,
+            },
+            {
+                'title':
+                    'Write a function <code>f</code> (<em>without</em> '
+                    ' the type annotation) which converts a'
+                    ' list of integers into a list of string. For instance'
+                    ' <code>f [2, 3, 5, 7] == ["2", "3", "5", "7"]</code>.'
+                    ' All the functions you need are in the slides.',
+                'coefficient': 1,
+            },
+            {
+                'title':
+                    '<code>ageStr</code> is a string entered by a user.'
+                    ' Define a variable <code>ageInt</code> which is'
+                    ' the conversion of <code>ageStr</code> in <code>Int</code'
+                    ' if it is a correct representation of an <code>Int</code>'
+                    ' or <code>-1</code> otherwise.',
+                'coefficient': 1,
+            },
+
+        ],
     },
 ]
 
 with app.app_context():
     db.create_all()
     for chapter in CHAPTERS:
+        for question in chapter['questions']:
+            question['grades_by_answer'] = {}
         try:
             db.session.merge(Chapter(**chapter))
             db.session.commit()

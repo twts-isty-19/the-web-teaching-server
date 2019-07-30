a9b6bd456ed0cabf782cf9ad2d6f62fda5aeb554
Sébastien Besnier <sebastien.fabrice.besnier@gmail.com>
2019-07-30 15:48:55 +0200

    updated mock users creation with a teacher

--- webapp/scripts/create_mock_users.py
+++ webapp/scripts/create_mock_users.py
@@ -11,18 +11,30 @@ from blueprints.users import User
 password = "12345"
 email = "test@example.fr"
 
-u1 = User(
-    email=email,
-    name="Toto",
-)
-u1.set_password(password)
+users = [
+    ("stud@dom.org", "Student", False),
+    ("teacher@dom.org", "Teacher", True)
+]
 
+users_inserted = []
 with app.app_context():
     db.create_all()
-    db.session.add(u1)
-    try:
-        db.session.commit()
-    except IntegrityError:
-        db.session.rollback()
+    for (email, name, is_teacher) in users:
+        user = User(
+            email=email,
+            name=name,
+            is_teacher=is_teacher,
+        )
+        user.set_password(password)
+        db.session.add(user)
+        try:
+            db.session.commit()
+            users_inserted.append((email, name, is_teacher))
+        except IntegrityError:
+            db.session.rollback()
 
-print("User %s with password %s is in the DB" % (email, password))
+
+print("Users inserted in the DB with password %s:" % (password))
+for (email, name, is_teacher) in users_inserted:
+    print(email, name, ": Teacher" if is_teacher else ": Student")
+
